577e3cdca28812ebe7f37980346038e53eacd1db
Jelena Jankovic <jjankovic996@gmail.com>
2020-07-13 21:23:37 +0200

    Base routing set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router'
 import { AppComponent } from './app.component';
-import { from } from 'rxjs';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    RouterModule
+    RouterModule.forRoot([
+      {path:'welcome', component: WelcomeComponent},
+      {path:'', redirectTo: 'welcome', pathMatch: 'full'},
+      {path:'login', component: LoginComponent},
+      {path:'about', component: AboutComponent},
+      {path:'courses', component: CourseListComponent}
+    ])
   ],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent,
+    CourseListComponent,
+    AboutComponent
+  ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
